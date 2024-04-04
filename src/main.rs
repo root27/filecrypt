a72d3eca9e08e1b600e21a650909b14ad1ec25d8
root27 <oguzhaneee@gmail.com>
2024-04-04 15:26:43 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
 
+
 use clap::Parser;
-use ring::digest;
+use fernet::Fernet;
+use base64::{Engine as _, alphabet, engine::{self, general_purpose}};
 
 #[derive(Parser,Debug)]
 struct Cli {
     path: String,
+}
+
+
+fn check_key(key: &str) -> Vec<u8> {
+    
+    let decoded = base64::engine::general_purpose::URL_SAFE.decode(key).unwrap();
+
+    if decoded.len() != 32 {
+
+        let mut new_decoded = Vec::new();
+
+        for i in 0..32 {
+            new_decoded.push(decoded[i]);
+        }
+
+        return new_decoded;
+
+
+        
+    }
+
+    return decoded;
+
 }
 
 
@@ -15,16 +40,26 @@ fn main() {
 
     let content = std::fs::read_to_string(&arg.path).expect("could not read file");
 
+    println!("File content: {}", content);
 
-    println!("File Content is: ");
+    _ = content; 
 
-    println!("{}", content);
+    let  my_key = String::from("my_key");
 
-    let bytesofcontent = content.as_bytes();
-
-    let hash = digest::digest(&digest::SHA256, bytesofcontent);
-
-    println!("Hash of the file is {:?}", hash);
     
+    let base64_encoded=  base64::engine::general_purpose::URL_SAFE.encode(my_key.as_bytes());
+
+    println!("Encoded Key is: {}", base64_encoded);
+
+    let newkey = check_key(&base64_encoded);
+
+    println!("New Key is: {:?}", newkey);
+   
+
+    // let fernet = Fernet::new(&newkey).unwrap();
+
+    // _ = fernet
+   
+
 
 }
