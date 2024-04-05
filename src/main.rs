ed6926259ede7529fcdb1c7b535d4a0e5872ed3f
root27 <oguzhaneee@gmail.com>
2024-04-05 11:39:54 +0300

    fernet enc,dec

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,54 @@
 
 
-use clap::Parser;
+use clap::{ArgAction, Command};
 use fernet::Fernet;
-use base64::{Engine as _, alphabet, engine::{self, general_purpose}};
-
-#[derive(Parser,Debug)]
-struct Cli {
-    path: String,
-}
+use std::fs;
 
 
-fn check_key(key: &str) -> Vec<u8> {
-    
-    let decoded = base64::engine::general_purpose::URL_SAFE.decode(key).unwrap();
-
-    if decoded.len() != 32 {
-
-        let mut new_decoded = Vec::new();
-
-        for i in 0..32 {
-            new_decoded.push(decoded[i]);
-        }
-
-        return new_decoded;
-
-
-        
-    }
-
-    return decoded;
-
-}
 
 
 fn main() {
 
-    let arg = Cli::parse();
+
+    let cmd = Command::new("File Encryptor/Decryptor")
+        .author("O.D")
+        .version("1.0.0")
+        .about("Encrypt and decrypt files using Fernet")
+        .arg(
+            clap::Arg::new("file")
+                .short('f')
+                .long("file")
+                .action(ArgAction::Set)
+            )
+        .arg(
+            clap::Arg::new("encrypt")
+                .short('e')
+                .long("encrypt")
+                .action(ArgAction::SetTrue)
+        )
+        .get_matches();
 
 
-    let content = std::fs::read_to_string(&arg.path).expect("could not read file");
 
-    println!("File content: {}", content);
+        if cmd.args_present() {
+            
+            if let Some(path) = cmd.get_one::<String>("file") {
+                println!("Path: {}", path);
+            
 
-    _ = content; 
-
-    let  my_key = String::from("my_key");
-
+            let content = std::fs::read_to_string(path).expect("could not read file");
+            let key = Fernet::generate_key();
+            let fernet = Fernet::new(&key).unwrap();
+            let token = fernet.encrypt(content.as_bytes());
+            fs::write(path, token).expect("could not write file");
+            let new_content = std::fs::read_to_string("test.txt").expect("could not read file");
+            println!("File content: {}", new_content);
+            let decryption = fernet.decrypt(&new_content).unwrap();
+            let decrypted_content = String::from_utf8(decryption).unwrap();
+            println!("Decrypted content: {}", decrypted_content);
+            }
+        }
     
-    let base64_encoded=  base64::engine::general_purpose::URL_SAFE.encode(my_key.as_bytes());
-
-    println!("Encoded Key is: {}", base64_encoded);
-
-    let newkey = check_key(&base64_encoded);
-
-    println!("New Key is: {:?}", newkey);
-   
-
-    // let fernet = Fernet::new(&newkey).unwrap();
-
-    // _ = fernet
-   
 
 
 }
