69b7d84684809d12c9c338bae84982fe23279bdd
root27 <oguzhaneee@gmail.com>
2024-04-05 12:27:37 +0300

    key flag issues fix

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use clap::{ArgAction, Command};
 use fernet::Fernet;
-use std::fs;
+use std::{default, fs};
 
 
 
@@ -38,12 +38,7 @@ fn main() {
                 .long("key")
                 .action(ArgAction::Set)
         )
-        .arg(
-            clap::Arg::new("Encrypted file")
-                .short('E')
-                .long("Encrypted file")
-                .action(ArgAction::Set)
-        )
+       
         .get_matches();
 
 
@@ -51,49 +46,45 @@ fn main() {
         if cmd.args_present() {
             
             if let Some(path) = cmd.get_one::<String>("file") {
-                println!("Path: {}", path);
             
-            if cmd.get_flag("encrypt") {
+                if cmd.get_flag("encrypt") {
 
-                let content = std::fs::read_to_string(path).expect("could not read file");
-                let key = Fernet::generate_key();
-                let fernet = Fernet::new(&key).unwrap();
-                let token = fernet.encrypt(content.as_bytes());
-                fs::write(path, token).expect("could not write file");
+                    let content = std::fs::read_to_string(path).expect("could not read file");
+                    let key = Fernet::generate_key();
+                    let fernet = Fernet::new(&key).unwrap();
+                    let token = fernet.encrypt(content.as_bytes());
+                    fs::write(path, token).expect("could not write file");
 
-                println!("File encrypted");
+                    println!("File encrypted");
 
-                println!("Please save the key for decryption: {}", key);
+                    println!("Please save the key for decryption: {}", key);
 
-            }
-
-            if cmd.get_flag("decrypt") {
-
-                if !cmd.get_flag("key"){
-                    println!("Please provide a key for decryption");
-                    return;
                 }
 
-                if !cmd.get_flag("Encrypted file") {
-                    
-                    println!("Please provide an encrypted file for decryption");
+                if cmd.get_flag("decrypt") {
 
-                    return;
+                    if let Some(_key) = cmd.get_one::<String>("key") {
+
+                        let key = cmd.get_one::<String>("key").unwrap();
+
+                        let encrypted_file = cmd.get_one::<String>("file").unwrap();
+
+                        let fernet = Fernet::new(&key).unwrap();
+                        
+                        let new_content = std::fs::read_to_string(encrypted_file).expect("could not read file");
+                        println!("File content: {}", new_content);
+                        let decryption = fernet.decrypt(&new_content).unwrap();
+                        let decrypted_content = String::from_utf8(decryption).unwrap();
+                        println!("Decrypted content: {}", decrypted_content);
+
+                        return;
+                    }
+
+
+                    println!("Please provide a key for decryption")
+
+
                 }
-
-            let key = cmd.get_one::<String>("key").unwrap();
-
-            let encrypted_file = cmd.get_one::<String>("Encrypted file").unwrap();
-
-            let fernet = Fernet::new(&key).unwrap();
-            
-            let new_content = std::fs::read_to_string(encrypted_file).expect("could not read file");
-            println!("File content: {}", new_content);
-            let decryption = fernet.decrypt(&new_content).unwrap();
-            let decrypted_content = String::from_utf8(decryption).unwrap();
-            println!("Decrypted content: {}", decrypted_content);
-            
-            }
         }
     }
     
